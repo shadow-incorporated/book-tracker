17e035b8b66de8342877f4f032730b2fa4e9603a
akashrajeshnair <nairakash2004@gmail.com>
2025-07-30 11:36:30 +0530

    #1: added login and registration pages

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router'
 import './index.css'
 import App from './App.tsx'
+import LoginPage from './pages/Login.tsx'
+import RegisterPage from './pages/Registration.tsx'
+import { Toaster } from 'sonner'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+      <Route path='/' element={<App />} />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/register' element={<RegisterPage />} />
+      </Routes>
+      <Toaster />
+    </BrowserRouter>
   </StrictMode>,
 )
